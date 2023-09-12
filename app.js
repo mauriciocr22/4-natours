14af5c6a4066cfae63d716afb018616542c9e1b1
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-09-12 18:59:47 -0300

    feat: handling delete request

--- app.js
+++ app.js
@@ -85,6 +85,20 @@ app.patch("/api/v1/tours/:id", (request, response) => {
   });
 });
 
+app.delete("/api/v1/tours/:id", (request, response) => {
+  if (request.params.id * 1 > tours.length) {
+    return response.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  response.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
