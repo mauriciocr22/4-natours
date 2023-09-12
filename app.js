57424f8e89b328b07efd0205265a00ebfe209e29
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-09-12 18:57:15 -0300

    feat: handling patch request

--- app.js
+++ app.js
@@ -69,6 +69,22 @@ app.post("/api/v1/tours", (request, response) => {
   );
 });
 
+app.patch("/api/v1/tours/:id", (request, response) => {
+  if (request.params.id * 1 > tours.length) {
+    return response.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  response.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...",
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
