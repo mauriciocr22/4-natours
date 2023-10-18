f9494cb75e016addf9c4b40ec1624f8a46f8ac86
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-10-18 18:47:55 -0300

    feat: connecting database with app

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({
   path: "./config.env",
 });
 
-const app = require("./app");
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connection successful!"));
 
 const port = process.env.PORT;
 app.listen(port, () => {
