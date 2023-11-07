f0044255eb9a4761d8b418f3b681fb0b9360dcf3
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-11-07 16:37:46 -0300

    feat: aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require("../models/tourModel");
 
+exports.aliasTopTours = (request, response, next) => {
+  request.query.limit = "5";
+  request.query.sort = "-ratingsAverage,price";
+  request.query.fields = "name,price,ratingsAverage,difficulty";
+  next();
+};
+
 exports.getAllTours = async (request, response) => {
   try {
     // Filtering through params
