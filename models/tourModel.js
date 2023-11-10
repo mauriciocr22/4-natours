d4e4059ee0163207858680b1204ce9be1d78f3d1
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-11-10 19:07:47 -0300

    feat: aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -90,16 +90,20 @@ tourSchema.pre("save", function (next) {
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
-
   next();
 });
 
-// tourSchema.post(/^find/, function (docs, next) {
-//   console.log(`Query took ${Date.now() - this.start} miliseconds!`);
-//   console.log(docs);
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} miliseconds!`);
+  next();
+});
 
-//   next();
-// });
+// Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
