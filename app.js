bbc439c9070103717e9a0be5326f2fb6199ea527
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-11-16 18:15:36 -0300

    feat: creating global error handling middleware

--- app.js
+++ app.js
@@ -21,9 +21,25 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (request, response, next) => {
-  response.status(404).json({
-    status: "fail",
-    message: `Can't find ${request.originalUrl} on this server!`,
+  // response.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${request.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${request.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, request, response, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  response.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
