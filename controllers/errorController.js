35ad9ac83a41c29b32b62f2f9d355b1152ef22ef
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-11-23 18:54:07 -0300

    feat: handling errors in production x development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,35 @@
-module.exports = (err, request, response, next) => {
-  // console.log(err.stack);
+const sendErrorDev = (err, response) => {
+  response.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (err, response) => {
+  if (err.isOperational) {
+    response.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💥", err);
+
+    response.status(500).json({
+      status: "error",
+      message: "Something went wrong.",
+    });
+  }
+};
+
+module.exports = (err, request, response, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  response.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, response);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, response);
+  }
 };
