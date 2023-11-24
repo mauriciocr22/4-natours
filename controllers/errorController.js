d6db51b950aff35a7cd838ecfeeedc1aa6fcfedd
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-11-24 16:31:49 -0300

    feat: handling validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,8 +7,16 @@ const handleCastErrorDB = (err) => {
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
+
   const message = `Duplicate field value: ${value} Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+
   return new AppError(message, 400);
 };
 
@@ -48,6 +56,8 @@ module.exports = (err, request, response, next) => {
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, response);
   }
