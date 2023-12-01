916daa6fe98f74d9c9905347232e934031e83c64
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-12-01 18:18:17 -0300

    feat: managing password

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please tell us your name!"],
-    unique: true,
   },
   email: {
     type: String,
@@ -23,7 +23,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     reqruired: [true, "Please confirm your password."],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
