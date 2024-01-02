50b2979b44563997a022819ef002b954286b999a
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-02 18:41:44 -0300

    feat: created users sign up route with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken')
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.signup = catchAsync(async (request, response, next) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
 
   response.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
