969f34c90f7bfd18318a6093a883b5186b6c5e3f
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-18 19:28:16 -0300

    feat: sending mails using nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError")
+const sendEmail = require("../utils/email")
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -96,6 +97,28 @@ exports.forgotPassword = catchAsync(async (request, response, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false })
 
-})
+  const resetURL = `${request.protocol}://${request.get("host")}/api/v1/users/resetPassword/${resetToken}`;
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n
+                  If you did not forget your password, please ignore this email.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 min)",
+      message
+    });
+    response.status(200).json({
+      status: "success",
+      message:"Token sent to email!"
+    })
+  } catch(err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new AppError("There was an error sending the email. Try again later.", 500))
+  }
+  
+});
 
 exports.resetPassword = (request, response, next) => {}
