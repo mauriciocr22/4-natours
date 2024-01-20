659149456509337433315f134d59999f66775d12
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-20 14:10:05 -0300

    feat: limiting requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit")
 
 const AppError = require("./utils/appError");
 const errorHandler = require("./controllers/errorController");
@@ -13,6 +14,15 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour."
+});
+
+app.use("/api", limiter)
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
