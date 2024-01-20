45b9ac09fce9321fb6122d3ebd93bc4b8ab9ecd8
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-20 14:16:59 -0300

    feat: using helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit")
+const helmet = require("helmet")
 
 const AppError = require("./utils/appError");
 const errorHandler = require("./controllers/errorController");
@@ -9,7 +10,7 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
+app.use(helmet())
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -21,9 +22,12 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again in an hour."
 });
 
-app.use("/api", limiter)
+app.use("/api", limiter);
 
-app.use(express.json());
+app.use(express.json({
+  limit: "10kb"
+}));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((request, response, next) => {
