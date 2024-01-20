3313080de7642fda5f5ae588ee03ee31ada33410
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-20 14:26:57 -0300

    feat: preventing malicious attacks

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-const rateLimit = require("express-rate-limit")
-const helmet = require("helmet")
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const errorHandler = require("./controllers/errorController");
@@ -28,13 +30,13 @@ app.use(express.json({
   limit: "10kb"
 }));
 
+app.use(mongoSanitize());
+app.use(xss())
 app.use(express.static(`${__dirname}/public`));
-
 app.use((request, response, next) => {
   request.requestTime = new Date().toISOString();
   next();
 });
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
