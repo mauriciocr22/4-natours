63d41adcd7239b400e61060bb213052a57e68aac
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-01-29 20:14:48 -0300

    feat: adding missing authentication and authorization middlewares to routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,10 +5,27 @@ const authController = require("./../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/")
-  .get(reviewController.getAllReviews)
-  .post(authController.protect, authController.restrictTo("user"), reviewController.setTourUserIds, reviewController.createReview);
+router.use(authController.protect);
 
-router.route("/:id").get(reviewController.getReview).patch(reviewController.updateReview).delete(reviewController.deleteReview);
+router
+  .route("/")
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo("user"), 
+    reviewController.setTourUserIds, 
+    reviewController.createReview
+  );
+
+router
+  .route("/:id")
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo("user", "admin"), 
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo("user", "admin"), 
+    reviewController.deleteReview
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,11 +10,16 @@ router.post("/login", authController.login);
 
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
-router.patch("/updateMyPassword", authController.protect, authController.updatePassword)
-router.patch("/updateMe", authController.protect, userController.updateMe)
 
-router.get("/me", authController.protect, userController.getMe, userController.getUser)
-router.delete("/deleteMe", authController.protect, userController.deleteMe)
+router.use(authController.protect);
+
+router.patch("/updateMyPassword", authController.updatePassword);
+
+router.patch("/updateMe", userController.updateMe);
+router.get("/me", userController.getMe, userController.getUser);
+router.delete("/deleteMe", userController.deleteMe);
+
+router.use(authController.restrictTo("admin"));
 
 router
   .route("/")
