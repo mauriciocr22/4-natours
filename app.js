2c072b5ead40020474a8dca8b81955bb4423fdd9
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-03-11 14:25:08 -0300

    feat: enabling trust proxy

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
 
+app.set('trust proxy', true);
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
